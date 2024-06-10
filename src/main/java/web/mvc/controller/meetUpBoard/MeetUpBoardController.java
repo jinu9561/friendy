3a49aa0e633dba6306690c9e60346ad6b710003e
jaehyeongP <asso03089@naver.com>
2024-06-10 15:02:57 +0900

    fix :  partyBoard -> meetup

--- src/main/java/web/mvc/controller/meetUpBoard/MeetUpBoardController.java
+++ src/main/java/web/mvc/controller/meetUpBoard/MeetUpBoardController.java
@@ -15,24 +15,25 @@ import web.mvc.service.meetUpBoard.MeetUpBoardService;
 @Slf4j
 public class MeetUpBoardController {
 
-    private final MeetUpBoardService partyBoardService;
+    private final MeetUpBoardService meetUpBoardService;
 
 
     @PostMapping("/create")
-    public ResponseEntity<?> createPartyBoard(@RequestBody MeetUpBoardDTO partyBoardDTO){
+    public ResponseEntity<?> createPartyBoard(@RequestBody MeetUpBoardDTO meetUpBoardDTO){
         //입력된 타입이 안맞으면 @RequestBody에서 애초에 걸러져버림. .
 
-    String result =partyBoardService.createParty(partyBoardDTO);
+    String result =meetUpBoardService.createParty(meetUpBoardDTO);
         System.out.println(result+"결과");
 
-        partyBoardService.findByPartySeq();
+        meetUpBoardService.findByPartySeq();
 
         return ResponseEntity.status(HttpStatus.OK).body("성공");
     }
 
     @GetMapping ("/test")
     public void test(){
-        partyBoardService.findByPartySeq();
+//       meetUpBoardService.findByPartySeq();
+       meetUpBoardService.checkDeadLine();
     }
 
 
