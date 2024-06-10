b738d96eaaa9d67211fda27d140dc638614e0c50
jaehyeongP <143363272+jaehyeongP@users.noreply.github.com>
2024-06-10 14:58:51 +0900

    Delete src/main/java/web/mvc/repository/partyRoom/PartyBoardDetailImgRepository.java

--- src/main/java/web/mvc/repository/partyRoom/PartyBoardDetailImgRepository.java
+++
@@ -1,8 +0,0 @@
-package web.mvc.repository.partyRoom;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import web.mvc.entity.partyBoard.PartyDetailImg;
-
-public interface PartyBoardDetailImgRepository extends JpaRepository<PartyDetailImg, Long> {
-
-}
