7736414566e05bfd3b8d7041d04a9b0eae4f2efb
YOUHAN PARK <youhanbark@gmail.com>
2024-06-14 17:15:07 +0900

    fix : EventDEtailImg 삭제 메소드 추가

--- src/main/java/web/mvc/repository/event/EventDetailImgRepository.java
+++ src/main/java/web/mvc/repository/event/EventDetailImgRepository.java
@@ -1,6 +1,8 @@
 package web.mvc.repository.event;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import web.mvc.entity.event.EventDetailImg;
 
@@ -9,4 +11,10 @@ import java.util.List;
 public interface EventDetailImgRepository extends JpaRepository<EventDetailImg, Long> {
     @Query("select e from EventDetailImg e where e.event.eventSeq = ?1")
     public List<EventDetailImg> findByEventSeq(Long eventSeq);
+
+    @Modifying
+    @Transactional
+    @Query("delete from EventDetailImg ed where ed.eventDetailImgSeq = ?1")
+    int deleteByEventDetailImgSeq(Long eventDetailImgSeq);
+
 }
