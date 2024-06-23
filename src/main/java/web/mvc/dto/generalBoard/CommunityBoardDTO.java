d89a7460b15467758edeb06a71923bb3ebd0d0c8
Sparkling <tlwidacot125@gmail.com>
2024-06-24 05:26:15 +0900

    fix : 날짜가 제대로 나오게 JsonFormat설정 추가

--- src/main/java/web/mvc/dto/generalBoard/CommunityBoardDTO.java
+++ src/main/java/web/mvc/dto/generalBoard/CommunityBoardDTO.java
@@ -1,5 +1,6 @@
 package web.mvc.dto.generalBoard;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.springframework.stereotype.Component;
 import web.mvc.entity.generalBoard.CommunityBoard;
@@ -26,11 +27,13 @@ public class CommunityBoardDTO {
     private int boardType;  //0이 자유게시판, 1이 익명게시판
     private int boardLike; //추천수
     private String boardPwd;    //게시글 비밀번호
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss") //Json으로 보낼 때, LocalDateTime을 String으로 변환
     private LocalDateTime boardRegDate; //게시글 등록일
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime boardUpdateDate; //게시글 수정일
     private int commBoardCount; //조회수
     private List<ReplyDTO> replyList; //댓글 리스트
-
+    private int photoBoardCount;
     // 사용자가 보낸 DTO에서 엔티티로 변환하는 메서드
     public CommunityBoard toEntity(Users user) {
         return CommunityBoard.builder()
